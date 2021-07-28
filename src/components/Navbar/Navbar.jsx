13b31a301756ebf7f80291422ed0b5db0ee32e2a
John <coldlombax@gmail.com>
2021-07-28 15:35:20 +0100

    Nav now contains state deciding active item.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import { Nav, NavList, NavItem } from './NavbarElements';
 
 export default function Navbar() {
+  const [value, setValue] = useState(0);
   return (
     <Nav>
       <NavList>
-        <NavItem>ABOUT</NavItem>
-        <NavItem>PROJECTS</NavItem>
-        <NavItem>EXPERIENCE</NavItem>
+        <NavItem
+          isActive={value === 0}
+          onClick={() => {
+            setValue(0);
+          }}
+        >
+          ABOUT
+        </NavItem>
+        <NavItem
+          isActive={value === 1}
+          onClick={() => {
+            setValue(1);
+          }}
+        >
+          PROJECTS
+        </NavItem>
+        <NavItem
+          isActive={value === 2}
+          onClick={() => {
+            setValue(2);
+          }}
+        >
+          EXPERIENCE
+        </NavItem>
       </NavList>
     </Nav>
   );
 }
+
+// when a value changes, determine whether
--- src/components/Navbar/NavbarElements.jsx
+++ src/components/Navbar/NavbarElements.jsx
@@ -5,10 +5,12 @@ export const Nav = styled.nav`
     padding: 2em;
 `;
 
-export const NavList = styled.nav`
+export const NavList = styled.ul`
     display: flex;
     justify-content: flex-end;
     gap: 3em;
+
+    list-style: none;
 
     font-size: clamp(1em, 3vw, 1.4em);
 
@@ -17,6 +19,14 @@ export const NavList = styled.nav`
     }
 `;
 
-export const NavItem = styled.nav`
+export const NavItem = styled.li`
     cursor: pointer;
+
+    &::after {
+        content: '';
+        display: ${({ isActive }) => (isActive ? 'block' : 'none')};
+        background-color: #7FFF7C;
+        width: 100%;
+        height: 2px;
+    }
 `;
