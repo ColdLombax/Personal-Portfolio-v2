b5e6eba0d05e84102eb906bd510694a4648c699c
John <coldlombax@gmail.com>
2021-07-29 01:22:10 +0100

    Hero component added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Hero from './components/Hero/Hero';
 import Navbar from './components/Navbar/Navbar';
 
 export default function App() {
   return (
     <div className="container">
       <Navbar />
+      <Hero />
     </div>
   );
 }
